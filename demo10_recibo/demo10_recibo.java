046f784d65213a8c39f0da5a140f47d548584945
eduardoperez2022 <102707420+eduardoperez2022@users.noreply.github.com>
2022-06-02 14:25:10 -0300

    Agrega enunciado

--- demo10_recibo/demo10_recibo.java
+++ demo10_recibo/demo10_recibo.java
@@ -2,6 +2,22 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+-- Enunciado --
+-- Escribir un programa en java que lea los siguientes datos como texto cada uno:
+Nombre, importe, concepto y fecha
+Luego implementar una clase recibo, con un método de nombre construir. Este método 
+debe recibir los campos recién leídos como parámetros, e intercalarlos sobre una 
+plantilla de texto fijo existente. La plantilla es la siguiente:
+---
+Recibo – Fecha <fecha>
+Recibí del Sr/a <nombre> la cantidad de pesos <importe> por el concepto de <concepto>
+
+------------------                                                  -------------------
+Firma emisor                                                 Firma receptor
+
+El texto final construido debe quedar en una variable de la clase recibo.
+Luego imprimir en pantalla el resultado del texto construido.
+
  */
 package demo10_recibo;
 
