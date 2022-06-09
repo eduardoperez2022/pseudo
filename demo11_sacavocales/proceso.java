d4e04bc18881a085b5ba47a0da4ed81c0ee75817
eduardoperez2022 <102707420+eduardoperez2022@users.noreply.github.com>
2022-06-09 20:02:07 -0300

    Codigo completo

--- demo11_sacavocales/proceso.java
+++ demo11_sacavocales/proceso.java
@@ -16,15 +16,47 @@ public class proceso {
     public void saca_vocales(String pfrase) {
         // usar ciclo for y charAt
         int largo = pfrase.length();
+        String salida="";
         
-        
+        for (int i = 0; i < largo; i++) {
+            char unchar = pfrase.charAt(i);
+            if (unchar == 'a') {
+                salida = salida.concat("");
+            }
+            else if (unchar == 'e') {
+                salida = salida.concat("");
+            }
+            else if (unchar == 'i') {
+                salida = salida.concat("");
+            }
+            else if (unchar == 'o') {
+                salida = salida.concat("");
+            }
+            else if (unchar == 'u') {
+                salida = salida.concat("");
+            }
+            else {
+                salida = salida.concat(String.valueOf(unchar));
+            }
+        }
+        this.construido = salida;
     }
 
     public void saca_vocales2(String pfrase) {
         // usar String.replace
-        String frase2 = pfrase;
+        String salida=pfrase;
         
+        salida = salida.replace("a", "");
+        salida = salida.replace("e", "");
+        salida = salida.replace("i", "");
+        salida = salida.replace("o", "");
+        salida = salida.replace("u", "");
+        
+        this.construido = salida;
     }
-    
+
+    public String getConstruido() {
+        return construido;
+    }
     
 }
